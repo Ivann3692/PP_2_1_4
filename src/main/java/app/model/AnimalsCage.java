ee6c169a9c7dafe997d447897317d6e191d70259
Artem <ivann3692gmail.com>
2023-04-26 21:38:04 +0300

    spf

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,37 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal;
+    @Autowired
+    @Qualifier("cat")
+    private Animal animal1;
 
     @Autowired
-    private Animal animal;
+    private Timer timer;
+
 
     public void whatAnimalSay() {
-        System.out.println("Say:");
+        System.out.println("say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
-        System.out.println("________________________");
+        System.out.println(timer.getTime());
+        System.out.println("                        ");
+
+        System.out.println("say:");
+        System.out.println(animal1.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("                         ");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
